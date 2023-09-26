98bae2f433b047f1510a754ce45dd03c73c858ba
biruk ayalew <se.biruk.ayalew@gmail.com>
2023-09-26 13:44:55 +0300

    add themes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
 
 import Navbar from './components/Navbar'
+import Themes from './components/Themes'
 import Home from './pages/home/home'
 import About from './pages/about/about'
 import Portfolio from './pages/portfolio/Portfolio'
@@ -13,6 +14,7 @@ function App() {
 
   return <BrowserRouter>
     <Navbar />
+    <Themes />
     <Routes>
       <Route index element={<Home />} />
       <Route path='about' element={<About />} />
